4b7b7903452c63939d186848c80de172a42348e6
Danny G <danielg@Daniels-MBP-2.home>
2024-04-25 10:02:40 -0400

    change to public visibility

--- test/invariants/handlers/DN404Handler.sol
+++ test/invariants/handlers/DN404Handler.sol
@@ -64,7 +64,7 @@ contract DN404Handler is SoladyTest {
         return actors[_bound(seed, 0, actors.length - 1)];
     }
 
-    function approve(uint256 ownerIndexSeed, uint256 spenderIndexSeed, uint256 amount) external {
+    function approve(uint256 ownerIndexSeed, uint256 spenderIndexSeed, uint256 amount) public {
         // PRE-CONDITIONS
         address owner = randomAddress(ownerIndexSeed);
         address spender = randomAddress(spenderIndexSeed);
@@ -79,7 +79,7 @@ contract DN404Handler is SoladyTest {
         assertEq(dn404.allowance(owner, spender), amount, "Allowance != Amount");
     }
 
-    function transfer(uint256 fromIndexSeed, uint256 toIndexSeed, uint256 amount) external {
+    function transfer(uint256 fromIndexSeed, uint256 toIndexSeed, uint256 amount) public {
         // PRE-CONDITIONS
         address from = randomAddress(fromIndexSeed);
         address to = randomAddress(toIndexSeed);
@@ -124,7 +124,7 @@ contract DN404Handler is SoladyTest {
         uint256 fromIndexSeed,
         uint256 toIndexSeed,
         uint256 amount
-    ) external {
+    ) public {
         // PRE-CONDITIONS
         address sender = randomAddress(senderIndexSeed);
         address from = randomAddress(fromIndexSeed);
@@ -170,7 +170,7 @@ contract DN404Handler is SoladyTest {
         assertEq(totalSupplyBefore, totalSupplyAfter, "total supply before != total supply after");
     }
 
-    function mint(uint256 toIndexSeed, uint256 amount) external {
+    function mint(uint256 toIndexSeed, uint256 amount) public {
         // PRE-CONDITIONS
         address to = randomAddress(toIndexSeed);
         amount = _bound(amount, 0, 100e18);
@@ -196,7 +196,7 @@ contract DN404Handler is SoladyTest {
         assertEq(totalSupplyBefore + amount, totalSupplyAfter, "supply after != supply before + amount");
     }
 
-    function mintNext(uint256 toIndexSeed, uint256 amount) public {
+    function mintNext(uint256 toIndexSeed, uint256 amount) internal {
         // PRE-CONDITIONS
         address to = randomAddress(toIndexSeed);
         amount = _bound(amount, 0, 100e18);
@@ -240,7 +240,7 @@ contract DN404Handler is SoladyTest {
         assertEq(totalSupplyBefore + amount, totalSupplyAfter, "supply before +amount != supply after");
     }
 
-    function burn(uint256 fromIndexSeed, uint256 amount) external {
+    function burn(uint256 fromIndexSeed, uint256 amount) public {
         // PRE-CONDITIONS
         address from = randomAddress(fromIndexSeed);
         vm.startPrank(from);
@@ -263,7 +263,7 @@ contract DN404Handler is SoladyTest {
         assertEq(totalSupplyBefore, totalSupplyAfter + amount, "supply before != supply after + amount");
     }
 
-    function setSkipNFT(uint256 actorIndexSeed, bool status) external {
+    function setSkipNFT(uint256 actorIndexSeed, bool status) public {
         // PRE-CONDITIONS
         address actor = randomAddress(actorIndexSeed);
 
@@ -276,7 +276,7 @@ contract DN404Handler is SoladyTest {
         assertEq(isSkipNFT, status, "isSKipNFT != status");
     }
 
-    function approveNFT(uint256 ownerIndexSeed, uint256 spenderIndexSeed, uint256 id) external {
+    function approveNFT(uint256 ownerIndexSeed, uint256 spenderIndexSeed, uint256 id) public {
         // PRE-CONDITIONS
         address owner = randomAddress(ownerIndexSeed);
         address spender = randomAddress(spenderIndexSeed);
@@ -298,7 +298,7 @@ contract DN404Handler is SoladyTest {
     }
 
     function setApprovalForAll(uint256 ownerIndexSeed, uint256 spenderIndexSeed, uint256 id, bool approval)
-        external
+        public
     {
         // PRE-CONDITIONS
         address owner = randomAddress(ownerIndexSeed);
@@ -319,7 +319,7 @@ contract DN404Handler is SoladyTest {
         uint256 fromIndexSeed,
         uint256 toIndexSeed,
         uint32 id
-    ) external {
+    ) public {
         // PRE-CONDITIONS
         address sender = randomAddress(senderIndexSeed);
         address from = randomAddress(fromIndexSeed);
@@ -393,13 +393,45 @@ contract DN404Handler is SoladyTest {
         //  [FAIL. Reason: mint next went over burned ids: 13 == 13]
         // 	[Sequence]
         // 	sender=0x6c04248eEAd24C0a5327Da4857e085D599CADCbf addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=mint(uint256,uint256) args=[115792089237316195423570985008687907853269984665640564039457584007913129639934 [1.157e77], 15708559942556327919058285537 [1.57e28]]
-        this.mint(115792089237316195423570985008687907853269984665640564039457584007913129639934, 42556327918744114339);
+        mint(115792089237316195423570985008687907853269984665640564039457584007913129639934, 42556327918744114339);
         // 	sender=0x000000c600000000000000000000000100000003 addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=setAddToBurnedPool(bool) args=[true]
         setAddToBurnedPool(true);
         // 	sender=0x0000000000000000000000019689C70d4e933Ed5 addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=transfer(uint256,uint256,uint256) args=[3613691728762 [3.613e12], 97416 [9.741e4], 48535295050378615425577907356361192661480127285716589363808434126849 [4.853e67]]
-        this.transfer(3613691728762 , 97416 , 48535295050378615425577907356361192661480127285716589363808434126849);
+        transfer(3613691728762 , 97416 , 48535295050378615425577907356361192661480127285716589363808434126849);
         // 	sender=0x0000000000000000000000000000000000018158 addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=mintNext(uint256,uint256) args=[109965082468 [1.099e11], 22859867009474803402833385750641289444944960863979897771692941901824 [2.285e67]]
-        this.mintNext(109965082468, 22859867009474803402833385750641289444944960863979897771692941901824);
+        mintNext(109965082468, 22859867009474803402833385750641289444944960863979897771692941901824);
         //  invariantMirrorAndBaseRemainImmutable() (runs: 10315, calls: 103146, reverts: 1) 
+    }
+
+    function poc_owned_index_not_index() internal {
+        //  [FAIL. Reason: revert: ownedIndex != i]
+        //  [Sequence]
+        //  sender=0x0000001A0000000200fffFfffffFFfFffffFFfff addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=mintNext(uint256,uint256) args=[811048 [8.11e5], 723531000577036798685916213594008081532211033450479617 [7.235e53]]
+        mintNext(811048, 723531000577036798685916213594008081532211033450479617);
+        //  sender=0x9f0A6745f074AccA55be333e1baa4c15C343335e addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=setAddToBurnedPool(bool) args=[true]
+        setAddToBurnedPool(true);
+        //  sender=0x0000000000000000000000000000000000083704 addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=mint(uint256,uint256) args=[115792089237316195423570985008687907853269984665640564039457584007913129639935 [1.157e77], 115792089237316195423570985008687907853269984665640564039457584007913129639935 [1.157e77]]
+        mint(115792089237316195423570985008687907853269984665640564039457584007913129639935, 115792089237316195423570985008687907853269984665640564039457584007913129639935);
+        //  sender=0x000000000000000000000000000000000000C304 addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=burn(uint256,uint256) args=[930361141132617696600007231793931638960177866343637659138896228481 [9.303e65], 3092605490865279355099875312615797476301765343180589804646345 [3.092e60]]
+        burn(930361141132617696600007231793931638960177866343637659138896228481, 3092605490865279355099875312615797476301765343180589804646345);
+        //  sender=0x3C624f73Dac70934296cAB18f41303a8C29BFffb addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=approveNFT(uint256,uint256,uint256) args=[3, 37595224438518972901097519449128827215179 [3.759e40], 0]
+        approveNFT(3, 37595224438518972901097519449128827215179, 0);
+        //  sender=0x0000000000000000010000000000000000000001 addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=mintNext(uint256,uint256) args=[224591879611914513246196655613176644196566470039175167 [2.245e53], 203305108264189912242559986334148991836622876980543488 [2.033e53]]
+        mintNext(224591879611914513246196655613176644196566470039175167, 203305108264189912242559986334148991836622876980543488);
+        //  sender=0xEAc0d0B96d4f8790F187439aBBCb80255a6aE2e7 addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=mintNext(uint256,uint256) args=[1, 64760335289823728000213847395181035399356028 [6.476e43]]
+        mintNext(1, 64760335289823728000213847395181035399356028);
+        //  sender=0x0000000000000000000000000000000100000001 addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=transferFrom(uint256,uint256,uint256,uint256) args=[91353182317495452320 [9.135e19], 3792478065 [3.792e9], 317868 [3.178e5], 1084136 [1.084e6]]
+        transferFrom(91353182317495452320, 3792478065, 317868, 1084136);
+        //  sender=0x000000000000000000000000000000000005b310 addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=approveNFT(uint256,uint256,uint256) args=[1272559165939207953607506622486397 [1.272e33], 4557583612253692448658464381713338480089709506295375190769 [4.557e57], 115792089237316195423570985008687907853269984665640564039457584007913129639933 [1.157e77]]
+        approveNFT(1272559165939207953607506622486397, 4557583612253692448658464381713338480089709506295375190769, 115792089237316195423570985008687907853269984665640564039457584007913129639933);
+        //  sender=0x000000240000000102FfFffFFFFFffFFffFFFFFf addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=transferFromNFT(uint256,uint256,uint256,uint32) args=[115792089237316195423570985008687907853269984665640564039457584007913129639932 [1.157e77], 2, 0, 52063735 [5.206e7]]
+        transferFromNFT(115792089237316195423570985008687907853269984665640564039457584007913129639932, 2, 0, 52063735);
+        //  sender=0x0000000000000000000000000000000002b1bcAB addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=setApprovalForAll(uint256,uint256,uint256,bool) args=[8115095003376306228240655166834959053640252303729303656098547341719190957 [8.115e72], 49865171109948960495674332398227415025634 [4.986e40], 36911260965954574392165138108743385200777 [3.691e40], true]
+        setApprovalForAll(8115095003376306228240655166834959053640252303729303656098547341719190957, 49865171109948960495674332398227415025634, 36911260965954574392165138108743385200777, true);
+        //  sender=0x675c75301517D3832Cb0c2DcfDB3A46aFB54d80f addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=setUseExistsLookup(bool) args=[false]
+        setUseExistsLookup(false);
+        //  sender=0x00000000000000000000000000000000000139c4 addr=[test/invariants/handlers/DN404Handler.sol:DN404Handler]0xF62849F9A0B5Bf2913b396098F7c7019b51A820a calldata=mint(uint256,uint256) args=[3, 131816594966788261336111922731059090399600923988487938664410896425 [1.318e65]]
+        mint(3, 131816594966788261336111922731059090399600923988487938664410896425);
+        // invariantMirrorAndBaseRemainImmutable() (runs: 7441, calls: 111613, reverts: 1)
     }
 }
